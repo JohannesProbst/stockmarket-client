9855c0858190fa8bceb1bd05aaf494d3673e58f6
Johannes Probst <johannes.probst@authenticvision.com>
2016-12-22 21:13:46 +0100

    adds forgotten exceptions

---
+++ src/main/java/at/ac/fhsalzburg/exception/StockExchangeConnectionException.java
@@ -0,0 +1,8 @@
+package at.ac.fhsalzburg.exception;
+
+/**
+ * Created by Johan on 13.12.2016.
+ */
+public class StockExchangeConnectionException extends RuntimeException {
+
+}
---
+++ src/main/java/at/ac/fhsalzburg/exception/StockExchangeInputException.java
@@ -0,0 +1,7 @@
+package at.ac.fhsalzburg.exception;
+
+/**
+ * Created by Johan on 21.12.2016.
+ */
+public class StockExchangeInputException extends RuntimeException {
+}
